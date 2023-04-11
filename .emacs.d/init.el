c8376bbd0eb4a05b65306fdc7abb4ceeea01fd11
tetsu <draftone@rustvillage.com>
2023-04-11 22:05:54 +0900

    first

---
+++ .emacs.d/init.el
@@ -0,0 +1,4 @@
+;; ctrl-hをバックスペースに設定
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+(setq inhibit-splash-screen t)
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DOT_FILES=(.zshrc .emacs.d)
+for file in ${DOT_FILES[@]}
+do
+        ln -s $HOME/dotfiles/$file $HOME/$file
+done
